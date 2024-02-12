b706a662574fa4ca6c3f2f5f29fc3a86a6608cc2
Musicisra <159588980+Musicisra@users.noreply.github.com>
2024-02-11 22:33:37 -0500

    Update submit_form.php

--- PHP/submit_form.php
+++ PHP/submit_form.php
@@ -1,19 +1,25 @@
 <?php
-// Recibir los datos del formulario
-$nombre = $_POST['nombre'];
-$telefono = $_POST['telefono'];
-$correo = $_POST['correo'];
-$mensaje = $_POST['mensaje'];
+$name = $_POST['name'];
+$mail = $_POST['mail'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
 
-// Configurar el correo electrónico
-$destinatario = 'nayeli28oliva@gmail.com'; // Cambia esto al correo al que deseas enviar los datos
-$asunto = 'Nuevo mensaje de contacto desde el formulario';
-$cuerpoMensaje = "Nombre: $nombre\nTeléfono: $telefono\nCorreo: $correo\nMensaje: $mensaje";
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
 
-// Enviar el correo electrónico
-mail($destinatario, $asunto, $cuerpoMensaje);
+$message = "Este mensaje fue enviado por: " . $name . " \r\n";
+$message .= "Su e-mail es: " . $mail . " \r\n";
+$message .= "Teléfono de contacto: " . $phone . " \r\n";
+$message .= "Mensaje: " . $_POST['message'] . " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
 
-// Redireccionar de vuelta a la página del formulario o a una página de confirmación
-header('Location: index.html'); // Cambia esto a la URL adecuada
-exit();
+$para = 'nayeli28oliva@gmail.com';
+$asunto = 'Mensaje de';
+
+mail($para, $asunto, utf8_decode($message), $header);
+
+header("Location:index.html");
 ?>
+
